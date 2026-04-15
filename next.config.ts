6e191c0e28678e6d3dbf2b7e2bfd3a6a19aa38b7
AlmareLLC-Dev <alliancemanagementresources@gmail.com>
2026-04-15 21:06:23 +0200

    Refactor next.config.ts for ESLint and TypeScript settings
    Updated next.config.ts to include ESLint and TypeScript build error configurations.

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        hostname: "res.cloudinary.com",
-        protocol: "https",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // This will ignore ESLint errors during production builds
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // This will ignore TypeScript errors during production builds
     ignoreBuildErrors: true,
   },
-  output: 'standalone',
 };
 
 module.exports = nextConfig;
